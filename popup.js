c21d4af15ad365422576d4a2839ba007ccbbabd6
Dave Pedu <dave.j.pedu@hp.com>
2015-02-09 22:04:05 -0800

    Display favicons in extension tab

--- popup.js
+++ popup.js
@@ -178,11 +178,12 @@ function renderWindow(windowdata, winnum) {
 
 function renderTab(tabdata, tabnum, winnum) {
     var tab = element('li', {class:"clearfix"})
-    
+    if(tabdata.icon) {
+        tab.style.backgroundImage = "url(\""+tabdata.icon+"\")";
+    }
     var label = element('span', {class:"name-line", _parent:tab})
     var label_icon = element('span', {class:"name-icon", _parent:label, _html:(tabdata.sticky?'<i class="fa fa-thumb-tack"></i> ':'')})
     var label_name = element('span', {class:"name-string", _parent:label, _html:tabdata.title + " ("+tabdata.id+")"})
-    
     
     var options = element('div', {class:"options", _parent:tab})
     
